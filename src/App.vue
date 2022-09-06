7b260b218f5dc06d55876cd8ab5839a76fc59239
diego rangel <diego.rangel@uao.edu.co>
2022-09-06 18:12:53 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <SegundosHora />
+  
+  
+  
+  </template>
+
+<script>
+  import SegundosHora from './components/Hora.vue';
+    export default {
+      name: 'App',
+      components: {
+      SegundosHora,
+  }
+    }
+  </script>
